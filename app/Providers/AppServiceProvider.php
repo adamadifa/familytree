ddbe5ea7be8f498f62b1c00bdb9773106827ba5f
Adam Adifa <adamabdi.al.a@gmail.com>
2026-03-29 16:20:49 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,14 +20,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Paksa HTTPS jika diakses lewat proxy/aaPanel yang memiliki header HTTPS
-        if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] === 'on' || $_SERVER['HTTPS'] === 1) 
-            || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
+        // if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] === 'on' || $_SERVER['HTTPS'] === 1) 
+        //     || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+        //     \Illuminate\Support\Facades\URL::forceScheme('https');
+        // }
 
-        // Paksa Root URL sesuai config APP_URL (penting untuk validasi Signature)
-        if (!app()->runningInConsole()) {
-            \Illuminate\Support\Facades\URL::forceRootUrl(config('app.url'));
-        }
+        // // Paksa Root URL sesuai config APP_URL (penting untuk validasi Signature)
+        // if (!app()->runningInConsole()) {
+        //     \Illuminate\Support\Facades\URL::forceRootUrl(config('app.url'));
+        // }
     }
 }
